16d293c84f7a5150e934a3852b0a7cd948bb678e
Shah Md. Arshad Rahman Ziban <150821693+arshadziban@users.noreply.github.com>
2024-08-02 21:37:59 +0600

    Add files via upload

---
+++ SQLQuery0002.sql
@@ -0,0 +1,14 @@
+USE test01
+SELECT* FROM Salesman
+
+-- Rename the colum
+EXEC sp_rename 'Salesman.name', 'names', 'COLUMN';
+
+--display specific columns such as names and commissions for all salespeople.
+SELECT names,city
+FROM Salesman
+
+-- to identify the unique salespeople ID. Return salesman_id
+SELECT DISTINCT salesman_id
+FROM Salesman;
+ 
---
+++ SQLQuery0003.sql
@@ -0,0 +1,13 @@
+USE test01
+SELECT* FROM Salesman_Data;
+
+--Add new column
+ALTER TABLE Salesman
+ADD orders VARCHAR(255);
+
+-- Update column value
+UPDATE Salesman
+SET orders = '1'
+WHERE salesman_id = 5007;
+
+EXEC sp_rename 'Salesman', 'Salesman_Data';
